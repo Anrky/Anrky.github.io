1cf5e9e10b8782ee4b7313df7fe50a3e51be4aaf
Anrky <63459370+Anrky@users.noreply.github.com>
2020-08-31 17:37:27 +0530

    robofriends react project
    robofriends  react project

---
+++ build/precache-manifest.ff696888301bba0a9330695d11180feb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb1f2b8482b789159e07cd711e33e04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d6205f5f8aae4cfae7a7",
+    "url": "/static/css/main.384dac10.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6205f5f8aae4cfae7a7",
+    "url": "/static/js/main.ba3025ab.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
